a849d951c7f62e2476951cf301dc4cc6b44f8d86
bravecharm <charmaineselma@gmail.com>
2021-09-09 23:15:34 +0800

    Sec12 Admin Create and Edit Product Screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -8,6 +8,10 @@ import {
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_RESET,
 } from '../constants/productConstants'
 
 // will handle the product list which is in the homescreen
@@ -44,10 +48,7 @@ export const productDetailsReducer = (
   }
 }
 
-export const productDeleteReducer = (
-  state = { product: { reviews: [] } },
-  action
-) => {
+export const productDeleteReducer = (state = {}, action) => {
   switch (action.type) {
     case PRODUCT_DELETE_REQUEST:
       return { loading: true }
@@ -55,6 +56,21 @@ export const productDeleteReducer = (
       return { loading: false, success: true }
     case PRODUCT_DELETE_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_CREATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_CREATE_RESET:
+      return {}
     default:
       return state
   }
