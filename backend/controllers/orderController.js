ca413847d2f14e683b64e41997eab5b568120206
bravecharm <charmaineselma@gmail.com>
2021-09-10 17:29:25 +0800

    Sec12 Mark Order as Delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,6 +80,26 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc Update Order to delivered
+// @route GET /api/orders/:id/deliver
+// @access Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 // @desc Get logged in user orders
 // @route GET /api/orders/myorders
 // @access Private
@@ -105,4 +125,5 @@ export {
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToDelivered,
 }
