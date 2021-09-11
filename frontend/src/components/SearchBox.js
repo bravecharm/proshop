347d81ecc8c32803a11a40647093afd7e48a8f44
bravecharm <charmaineselma@gmail.com>
2021-09-11 18:23:49 +0800

    SearcBox layout issue resolved

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -15,7 +15,7 @@ const SearchBox = ({ history }) => {
   }
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} className='d-flex'>
       <Form.Control
         type='text'
         name='q'
